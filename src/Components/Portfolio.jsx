82632d824c6a1e7e28c947fa9e895cada28476a6
skywalker290 <dev.ayusshh@gmail.com>
2024-04-07 23:49:19 +0530

    test_change

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,7 +31,7 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "lesgooo guys",
     description:
       "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
     url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
